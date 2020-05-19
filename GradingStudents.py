f1735c0d6bed412bc5466a2827f6f22ee0840124
Nisargasreedhar <49960201+Nisargasreedhar@users.noreply.github.com>
2020-05-19 00:32:38 -0700

    Create GradingStudents.py

---
+++ GradingStudents.py
@@ -0,0 +1,44 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def gradingStudents(grades):
+    # Write your code here
+    f_grades=[]
+    for i in range(len(grades)):
+        try:
+            if grades[i]>37:
+                if grades[i]%5!=0:           
+                    if grades[i]%5>2:
+                        grades[i]+=(5-grades[i]%5)
+                        f_grades.append(grades[i])
+                    else:
+                        f_grades.append(grades[i])
+                else:
+                    f_grades.append(grades[i])
+            else:
+                f_grades.append(grades[i])
+        except:
+            break
+    return f_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
